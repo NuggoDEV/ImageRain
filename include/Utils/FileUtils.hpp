59447364b7d7f28a7b4f19af93229c62a0239def
NuggoDEV <NuggoDEV@gmail.com>
2023-02-01 17:42:24 +1030

    Image loading, still some little things to do :D

---
+++ include/Utils/FileUtils.hpp
@@ -0,0 +1,22 @@
+// TAKEN FROM OPTIMUS' MOD, IMAGE FACTORY. FILE LINKED BELOW!
+// https://github.com/OptimusChen/ImageFactory-Quest/blob/master/include/Utils/FileUtils.hpp
+
+#pragma once
+
+#include <string>
+
+#include "custom-types/shared/macros.hpp"
+#include "custom-types/shared/types.hpp"
+#include "System/IO/FileStream.hpp"
+
+namespace FileUtils {
+    bool isImageFile(std::string_view path);
+    std::string RemoveExtension(std::string path);
+    std::string GetFileName(std::string path, bool removeExtension);
+    std::vector<std::string> getFiles(std::string_view path);
+    bool isGifFile(std::string_view path);
+    bool isJsonFile(std::string_view path);
+    long GetFileSize(std::string_view filename, System::IO::FileStream* stream);
+    int FileSizeDivisor(long size);
+    std::string FileSizeExtension(long size);
+}  
--- src/ModUI/LeftImageSelectController.cpp
+++ src/ModUI/LeftImageSelectController.cpp
@@ -4,19 +4,67 @@
 #include "questui/shared/BeatSaberUI.hpp"
 using namespace QuestUI;
 
+#include "Utils/FileUtils.hpp"
+
+#include "UnityEngine/UI/LayoutElement.hpp"
+#include "UnityEngine/Vector2.hpp"
+using namespace UnityEngine;
+using namespace UnityEngine::UI;
+
 #include <vector>
 #include <filesystem>
 
 DEFINE_TYPE(ImageRain::UI, LeftImageSelectController);
 
+
+// TAKEN FROM OPTIMUS' MOD, IMAGE FACTORY!
+#define SetPreferredSize(identifier, width, height)                                         \
+    auto layout##identifier = identifier->get_gameObject()->GetComponent<LayoutElement*>(); \
+    if (!layout##identifier)                                                                \
+        layout##identifier = identifier->get_gameObject()->AddComponent<LayoutElement*>();  \
+    layout##identifier->set_preferredWidth(width);                                          \
+    layout##identifier->set_preferredHeight(height)
+
 void ImageRain::UI::LeftImageSelectController::DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
 {
+    SetName(this, "Left Side Images");
+
     if (!firstActivation) return;
     const std::string path = ModDir + "/Images";
 
     auto cont = BeatSaberUI::CreateVerticalLayoutGroup(get_transform());
 
-    auto text = BeatSaberUI::CreateText(cont->get_transform(), "Left Screen!");
-        text->set_alignment(TMPro::TextAlignmentOptions::Center);
+    //auto text = BeatSaberUI::CreateText(cont->get_transform(), "Left Screen!");
+      //  text->set_alignment(TMPro::TextAlignmentOptions::Center);
+    
+    std::vector<std::string> images = FileUtils::getFiles(path);
+
+    if (images.size() == 0) 
+    {
+        BeatSaberUI::CreateText(cont->get_transform(), "No images found!\nMake sure to add images to the path below:\n'sdcard/ModData/com.beatgames.beatsaber/Mods/ImageRain/Images'");
+    }
+    else
+    {
+        for (int i = 0; i < images.size(); i++) 
+        {
+            auto image = images.at(i);
+
+            auto levelBarLayout = BeatSaberUI::CreateHorizontalLayoutGroup(cont->get_transform());
+                levelBarLayout->set_childControlWidth(false);
+            
+            auto levelBarElement = levelBarLayout->GetComponent<LayoutElement *>();
+                levelBarElement->set_minHeight(10.0f);
+                levelBarElement->set_minWidth(20.0f);
+
+            auto sprite = BeatSaberUI::FileToSprite(image);
+            Object::DontDestroyOnLoad(sprite);
+
+            auto img = BeatSaberUI::CreateImage(levelBarElement->get_transform(), sprite,  Vector2(0.0f, 0.0f),  Vector2(10.0f, 2.0f));
+            SetPreferredSize(img, 10.0f, 2.0f);
+
+            BeatSaberUI::CreateText(levelBarElement->get_transform(), FileUtils::GetFileName(image, true), true);
+            levelBarElement->set_minWidth(1.0f);
+        }
+    }
 
 }
